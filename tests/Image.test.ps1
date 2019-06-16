d06930dfafd0d643fde4acd8132a83b53a4e09d3
Sam Cogan <mail@samcogan.com>
2019-06-16 13:49:56 +0100

    Add pester tests

---
+++ tests/Image.test.ps1
@@ -0,0 +1,17 @@
+Describe "Feature Validation"{
+    Context "Windows Features"{
+
+        It "should have Web-Server Role Installed"{
+            get-windowsfeature -name "Web-Server" | should be $true
+        }
+
+        It "should have Web-Mgmt-Service Role Installed"{
+            get-windowsfeature -name "Web-Mgmt-Service" | should be $true
+        }
+
+        It "should have Web-Asp-Net45 Role Installed"{
+            get-windowsfeature -name "Web-Asp-Net45" | should be $true
+        }
+        
+    }
+}
