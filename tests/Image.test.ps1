4c0dc226d7146849ed651d81f387697bef572a44
Sam Cogan <mail@samcogan.com>
2019-06-17 09:15:13 +0100

    Update Image.test.ps1

--- tests/Image.test.ps1
+++ tests/Image.test.ps1
@@ -13,9 +13,6 @@ Describe "Feature Validation"{
             get-windowsfeature -name "Web-Asp-Net45" | should be $true
         }
 
-        It "should not have Web-Asp-Net45 Role Installed"{
-            get-windowsfeature -name "Web-Asp-Net45" | should be $false
-        }
         
         
     }
